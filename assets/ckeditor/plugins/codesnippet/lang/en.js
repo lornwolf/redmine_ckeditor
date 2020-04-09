b7bb9c7dfa4525b5986b6664492e9abd716d8f79
zongzheng.song@ipanda.co.jp <zongzheng.song@ipanda.co.jp>
2020-04-09 19:00:53 +0900

    添加codemirror和表情插件

--- assets/ckeditor/plugins/codesnippet/lang/en.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.setLang( 'codesnippet', 'en', {
-	button: 'Insert Code Snippet',
-	codeContents: 'Code content',
-	emptySnippetError: 'A code snippet cannot be empty.',
-	language: 'Language',
-	style: 'theme',
-	title: 'Code snippet',
-	pathName: 'code snippet'
-} );
--- assets/ckeditor/plugins/codesnippet/lang/ja.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.setLang( 'codesnippet', 'ja', {
-	button: 'コードスニペットを挿入',
-	codeContents: 'コード内容',
-	emptySnippetError: 'コードスニペットを入力してください。',
-	language: '言語',
-	style: 'テーマ',
-	title: 'コードスニペット',
-	pathName: 'コードスニペット'
-} );
